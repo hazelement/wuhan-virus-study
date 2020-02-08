cca5fa7235a8e62feb00f4ae007a7e4c9cd962fc
Harry Zheng <harry.zheng@outlook.com>
2020-02-07 21:08:31 -0800

    Updated graph
    Added more data to wuhan virus. Filled in blank data with data from
    previous day.
    
    Added fitted formula to graph.

--- analysis.py
+++ analysis.py
@@ -9,13 +9,13 @@ def read_file(filename):
     return df.iloc[:, 1].astype(float).copy()
 
 
-def run_model(file_name, model):
+def run_model(file_name, model, expression):
     y = read_file(file_name)
     x = np.arange(0, len(y), 1)
 
     coeff, cov = curve_fit(model, x, y)
-
-    print(cov)
+    formula = expression(*coeff)
+    print(formula)
 
     x_1 = np.arange(0, len(x) * 1.2, 1)
     y_1 = [model(t, *coeff) for t in x_1]
@@ -23,11 +23,13 @@ def run_model(file_name, model):
     plt.scatter(x, y)
     plt.plot(x_1, y_1, color="red")
     plt.title(file_name)
+    plt.text(1, y.values[-1], formula, fontsize=12)
     plt.savefig(file_name.split(".")[0] + ".png")
 
 
 if __name__ == '__main__':
-    file_name = "sars.csv"
+    file_name = "wuhan.csv"
     from models import si_model as model
+    from models import si_model_expression as expression
 
-    run_model(file_name, model)
+    coeff = run_model(file_name, model, expression)
